3bed12037a75c4cc989bf3e304b5d8e798573997
Seong-Rak Choi <ragiragi@hanmail.net>
2011-11-09 14:49:18 +0900

    fix popup.js build error

--- daumeditor/js/popup.js
+++ daumeditor/js/popup.js
@@ -31,19 +31,25 @@ var CORE_FILES = [
     "popuputil.js"
 ];
 
-var urlParams = {};
-(function () {
-    var e,
-        a = /\+/g,  // Regex for replacing addition symbol with a space
-        r = /([^&=]+)=?([^&]*)/g,
-        d = function (s) { return decodeURIComponent(s.replace(a, " ")); },
-        q = window.location.search.substring(1);
+try {
+    var urlParams = {};
+    (function () {
+        var e,
+            a = /\+/g,  // Regex for replacing addition symbol with a space
+            r = /([^&=]+)=?([^&]*)/g,
+            d = function (s) {
+                return decodeURIComponent(s.replace(a, " "));
+            },
+            q = window.location.search.substring(1);
 
-    while (e = r.exec(q))
-       urlParams[d(e[1])] = d(e[2]);
-})();
-if (urlParams.xssDomain) {
-    document.domain = urlParams.xssDomain;
+        while (e = r.exec(q))
+            urlParams[d(e[1])] = d(e[2]);
+    })();
+    if (urlParams.xssDomain) {
+        document.domain = urlParams.xssDomain;
+    }
+} catch (e) {
+    // ignore error when loaded from build script
 }
 
 try {
