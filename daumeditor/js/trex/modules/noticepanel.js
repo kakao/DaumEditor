29741a34229a5f07b0e574705d0fcc27dbeafb2b
Seong-A Kong <simonz@daumcorp.com>
2012-12-04 18:34:57 +0900

    fix noticepanel.js (not hiding, not focused, disappear when focused) FTDUEDTR-1299

--- daumeditor/js/trex/modules/noticepanel.js
+++ daumeditor/js/trex/modules/noticepanel.js
@@ -1,25 +1,26 @@
 Trex.module("Add layer to display notice message on editor area before editing", //NOTE: #FTDUEDTR-18
-	function(editor, toolbar, sidebar, canvas, config){
-		if ( config.initializedMessage ) {
-			canvas.observeJob(Trex.Ev.__IFRAME_LOAD_COMPLETE,  function(){
-				var _noticeDiv = tx.div({ id: "tx-canvas-notice", className: "tx-canvas-notice"}, config.initializedMessage);
-				$tx("tx_canvas").insertBefore( _noticeDiv, $tx("tx_loading") );
-				
-				var _noticeDivHandler = function(){
-					if ($tx("tx-canvas-notice")) {
+	function (editor, toolbar, sidebar, canvas, config) {
+		if (config.initializedMessage) {
+			canvas.observeJob(Trex.Ev.__IFRAME_LOAD_COMPLETE, function () {
+				var _noticeDiv = tx.div({ id:"tx-canvas-notice", className:"tx-canvas-notice"}, config.initializedMessage);
+				$tx("tx_canvas").insertBefore(_noticeDiv, $tx("tx_loading"));
+
+				var disappeared = false;
+				var _noticeDivHandler = function () {
+					if (!disappeared && $tx("tx-canvas-notice")) {
+						disappeared = true;
 						$tx("tx_canvas").removeChild(_noticeDiv);
-						if (editor.focus) {
-							editor.focus();
-						}
+						canvas.focus();
 					}
 				};
-				
+
+				setTimeout(function () {
+					$tx.observe(canvas.getPanel('html').getWindow(), 'focus', _noticeDivHandler);
+				}, 30);
 				$tx.observe(_noticeDiv, "click", _noticeDivHandler);
 				canvas.observeJob(Trex.Ev.__CANVAS_DATA_INITIALIZE, _noticeDivHandler);
-				toolbar.observeJob(Trex.Ev.__TOOL_CLICK,  _noticeDivHandler);
+				toolbar.observeJob(Trex.Ev.__TOOL_CLICK, _noticeDivHandler);
 			});
-		
 		}
-		
 	}
 );
