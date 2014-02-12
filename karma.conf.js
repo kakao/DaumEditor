3e4bf2e7300e6ebd5d584de4b4fe4f6c3965b5cf
sungwon <sungwon@daumcorp.com>
2014-02-12 13:58:09 +0900

    testcase loader chage (header.js use)

--- karma.conf.js
+++ karma.conf.js
@@ -248,7 +248,7 @@ module.exports = function(config) {
 
     // test results reporter to use
     // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
 
 
     // web server port
