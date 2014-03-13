b839366de44524120e6338b72ac1fbed35c2eb92
sungwon <sungwon@daumcorp.com>
2014-03-13 19:14:42 +0900

    autosize ff bug fix

--- daumeditor/js/trex/modules/autosize.js
+++ daumeditor/js/trex/modules/autosize.js
@@ -11,28 +11,35 @@ Trex.module("auto canvas height resize",
             var _panel = canvas.getCurrentPanel();
             if(!canvas.isWYSIWYG()) return;
             var _doc = _panel.getDocument();
-            var _body = _doc.body;
-            $tx.setStyle(_body,{
-                position: 'absolute'
-            });
-            var _h = _body.offsetHeight ;
-            //스크롤 여부 확인 body 높이가 > maxHeight 일경우
-            if(_h >= _config.maxHeight)
+            var _h;
+            if($tx.msie){
+                var _body = _doc.body;
                 $tx.setStyle(_body,{
-                    overflow: ''
+                    position: 'absolute'
                 });
-            else {
-                $tx.setStyle(_body,{
-                    overflow: 'hidden'
-                });
-            }
-            //높이 변경이 없는 경우
-            if(_h == beforeHeight) {
+                _h = _body.offsetHeight ;
+                //스크롤 여부 확인 body 높이가 > maxHeight 일경우
+                if(_h >= _config.maxHeight)
+                    $tx.setStyle(_body,{
+                        overflow: ''
+                    });
+                else {
+                    $tx.setStyle(_body,{
+                        overflow: 'hidden'
+                    });
+                }
+                //높이 변경이 없는 경우
                 $tx.setStyle(_body,{
                     position: ''
                 });
+            }else {
+                _h = _doc.documentElement.offsetHeight;
+            }
+
+            if(_h == beforeHeight) {
                 return;
             }
+
             var _height = Math.min(Math.max(parseInt(_h, 10), _config.minHeight), _config.maxHeight).toPx();
             _panel.setPanelHeight(_height);
             canvas.fireJobs('canvas.height.change', _height);
