dd0c7c6850bbbc6e0943b584206e4d2edd6203dc
jinwoo choi <maart@daumcorp.com>
2014-05-08 11:55:34 +0900

    table in the table, set width to 100%.

--- daumeditor/js/trex/tool/table.js
+++ daumeditor/js/trex/tool/table.js
@@ -1,7 +1,7 @@
 /**
- * @fileoverview 
+ * @fileoverview
  *  Table을 삽입하는 기능을 가진 Tool 'table' Source,
- *  Class Trex.Tool.Table,Trex.Menu.Table,Trex.Menu.Table.TableEdit와 configuration 을 포함 하고있다.    
+ *  Class Trex.Tool.Table,Trex.Menu.Table,Trex.Menu.Table.TableEdit와 configuration 을 포함 하고있다.
  */
 
 TrexConfig.addTool(
@@ -44,15 +44,26 @@ Trex.Tool.Table = Trex.Class.create({
 	oninitialized: function(config) {
 		var _self = this;
 		this.tableSize = { row: 0, col:0 };
-		
+
 		var _canvas = this.canvas;
 
 		var _toolHandler = this.handler = function(data) {
-			var table = _self.makeEmptyTable(data.row, data.col);
 			_canvas.execute(function(processor) {
-				var _tNode = processor.pasteContent(table, _TRUE);
-				processor.bookmarkInto(_tNode);
-				
+                // get table width
+                var tableWidth = '100%';
+                processor.executeUsingCaret(function(range, savedCaret){
+                    var endCaret = savedCaret.getCaret(_FALSE);
+                    if (!$tom.ancestor(endCaret, 'table')) {
+                        tableWidth = _self.getDefaultTableWidth();
+                    }
+                });
+
+                // insert empty table
+                var table = _self.makeEmptyTable(data.row, data.col, tableWidth);
+                var _tNode = processor.pasteContent(table, _TRUE);
+                processor.bookmarkInto(_tNode);
+
+                // open advanced toolbar
 				if (_self.toolbar.tools.advanced) {
 					_self.toolbar.tools.advanced.forceOpen();
 				}
@@ -69,11 +80,11 @@ Trex.Tool.Table = Trex.Class.create({
 			_toolHandler
 		);
 	},
-	makeEmptyTable: function(row, col){
+	makeEmptyTable: function(row, col, tableWidth){
 		var tableStringArr = [];
-		var tableWidth = this.getDefaultTableWidth();
 
-        tableStringArr.push("<table class=\""+Trex.Tool.Table.__DEFAULT_TABLE_CLASS+"\" width=\"" + tableWidth + "\" ");
+        tableStringArr.push("<table class=\""+Trex.Tool.Table.__DEFAULT_TABLE_CLASS+"\"");
+        tableWidth && tableStringArr.push(' width="' + tableWidth + '"');
 		tableStringArr.push(Trex.Tool.Table.__DEFAULT_TABLE_PROPERTY_STR);
 		tableStringArr.push(" style=\"");
 		tableStringArr.push(Trex.Tool.Table.__DEFAULT_TABLE_STYLE);
@@ -82,12 +93,12 @@ Trex.Tool.Table = Trex.Class.create({
 		tableStringArr.push(";font-size:");
 		tableStringArr.push( this.editor.canvas.getStyle("fontSize") );
 		tableStringArr.push("\"><tbody>");
-		
+
 		var borderStyleText = this.config.borderStyle;
-		var tdWidth = this.getDefaultCellWidth(col),
+		var tdWidth = this.getDefaultCellWidth(col, tableWidth),
             tdHeight = this.getDefaultCellHeight(row);
 		var basicBorder = ["border-bottom:",borderStyleText,";border-right:",borderStyleText,";"].join("");
-		
+
 		for( var i = 0; i < row; i++ ){
 			tableStringArr.push("<tr>");
 			for( var j = 0; j < col; j++ ){
@@ -108,11 +119,11 @@ Trex.Tool.Table = Trex.Class.create({
 			tableStringArr.push("</tr>")
 		}
 		tableStringArr.push("</tbody></table>");
-		return tableStringArr.join(""); 	
+		return tableStringArr.join("");
 	},
-    getDefaultCellWidth: function(columnCount) {
-        var tableWidth = this.getDefaultTableWidth();
+    getDefaultCellWidth: function(columnCount, tableWidth) {
         var width;
+        tableWidth = tableWidth || '100%';
         if (tableWidth.toString().indexOf('%') !== -1) {
             width = (100 / columnCount) + '%';
         } else {
@@ -173,20 +184,20 @@ Trex.Tool.Table.TemplateWizard = Trex.Class.create({
 		if ( isNaN( templateIndex ) ){
 			return ;
 		}
-		
+
 		var tableMatrixer = new Trex.Tool.Table.TableCellMatrixer(table);
 		var tdMatrix = tableMatrixer.getTdMatrix();
-					
+
 		this.currentTemplate = this.templateList[templateIndex];
 		for( var i = 0; i < tdMatrix.length; i++){
 			for( var j = 0; j < tdMatrix[i].length; j++){
 				this.setCellStyle(tdMatrix[i][j], {
 					isEvenRow: (i % 2) == 1,
 					isFirstRow: i == 0,
-					isLastRow: i == tdMatrix.length - 1, 
+					isLastRow: i == tdMatrix.length - 1,
 					isFirstCol: j == 0,
-					isLastCol: (j == tdMatrix[i].length - 1)  
-				});	
+					isLastCol: (j == tdMatrix[i].length - 1)
+				});
 			}
 		}
 	},
@@ -207,10 +218,10 @@ Trex.Tool.Table.TableCellMatrixer = Trex.Class.create({
 	initialize: function(table){
 		this.rowSize = this.initRowSize(table);
 		this.colSize = this.initColSize(table);
-		var context = $tom.first(table,"tbody") || table;				
+		var context = $tom.first(table,"tbody") || table;
 		this.tdMatrix = this.createTdMatrix(context);
 		//this.table = table;
-		
+
 		for (var i = 0; i < this.tdMatrix.length; i++) {
 			for (var j = 0; j < this.tdMatrix[i].length; j++) {
 				var td = this.tdMatrix[i][j];
@@ -226,24 +237,24 @@ Trex.Tool.Table.TableCellMatrixer = Trex.Class.create({
 				if ( td.rows > 1 ){
 					td.rows--;
 					this.tdMatrix[i + 1].splice(j, 0, td);
-				}	
+				}
 			}
 		}
 	},
 
 	createTdMatrix: function(tbody){
 		var tdMatrix = [];
-	
+
 		var trArr = $tom.children(tbody, "tr");
 		for( var i = 0, len = trArr.length; i < len; i++ ){
 			tdMatrix.push(this.createTdArray(trArr[i]));
 		}
-	
+
 		return tdMatrix;
 	},
 	createTdArray: function(tr){
 		var tdArr = [];
-			
+
 		var tds = $tom.children(tr, "td");
 		for( var i = 0, len = tds.length; i < len; i++ ){
 			tdArr.push( this.decorateSingleTd( tds[i] ) );
@@ -252,14 +263,14 @@ Trex.Tool.Table.TableCellMatrixer = Trex.Class.create({
 	},
 	decorateSingleTd: function(td){
 		var cols = parseInt( td.getAttribute("colSpan") || 1);
-		var rows = parseInt( td.getAttribute("rowSpan") || 1); 
+		var rows = parseInt( td.getAttribute("rowSpan") || 1);
 		td.cols = cols;
 		td.rows = (rows-1) * cols + 1;
 
 		return td;
 	},
 	initRowSize: function(table){
-		return table.rows.length;	
+		return table.rows.length;
 	},
 	initColSize: function(table){
 		var colSize = 0;
@@ -306,13 +317,13 @@ Trex.Menu.Table = Trex.Class.create({
 	ongenerated: function() {
 		this.rowSize = 1;
 		this.colSize = 1;
-		
+
 		this.elInnerPreview = $tom.collect(this.elMenu, 'div.tx-menu-inner .tx-menu-preview');
 		this.dynamicSizer = this.generateDynamicSizer(this.elInnerPreview);
-		
+
 		this.elInnerRowCol = $tom.collect(this.elMenu, 'div.tx-menu-inner .tx-menu-rowcol');
 		this.generateTextSizer(this.elInnerRowCol);
-		
+
 		this.elButtonArea = $tom.collect(this.elMenu, 'div.tx-menu-inner .tx-menu-enter');
 		this.generateButtonArea(this.elButtonArea);
 	},
@@ -335,7 +346,7 @@ Trex.Menu.Table = Trex.Class.create({
 		var elRowCol= tx.span();
 		var elDisplay = tx.div({className:"tx-dynamic-sizer-display"}, TXMSG('@table.title.insert'), elRowCol);
 		elPreivewContext.appendChild( elDisplay );
-		
+
 		var dynamicSizer = new Trex.DynamicSizer({
 			el: elPreivewContext,
 			clickHandler: this.onSelect.bind(this),
@@ -343,7 +354,7 @@ Trex.Menu.Table = Trex.Class.create({
 				elRowCol.innerHTML = row + 'x' +col;
 			}
 		});
-		
+
 		var _elA = tx.a({href:"javascript:;"}, TXMSG('@table.title.setDirectly'));
 		$tx.observe( _elA, "click", function(ev){
 			_self.showTextSizer();
@@ -354,18 +365,18 @@ Trex.Menu.Table = Trex.Class.create({
                 }
             });
 		});
-		
+
 		var _elButton = tx.div({className:"tx-more-button"});
 		_elButton.appendChild(_elA);
 		elPreivewContext.appendChild( _elButton );
-		
+
 		return dynamicSizer;
 	},
 	generateTextSizer: function(elContext) {
 		var _self = this;
-		
+
 		Trex.MarkupTemplate.get('menu.table.direct').evaluateToDom({}, elContext);
-		
+
 		var calculator = {
 			calculate: function(value, max, operand){
 				value = parseInt(value);
@@ -382,33 +393,33 @@ Trex.Menu.Table = Trex.Class.create({
 					return previousValue;
 				}else{
 					return value;
-				}		
-				
+				}
+
 			}
 		};
-		
+
 		var colInput = $tom.collect(elContext, "div.tx-col-field input");
 		$tx.observe(colInput, "blur", function(){
 			colInput.value = _self.colSize = calculator.getValidValue(colInput.value, _self.colSize, Trex.Menu.Table.MAX_COL);
 		});
-		$tx.observe( $tom.collect(elContext, "div.tx-col-field a.tx-btn-add"), "click", function(){ 
+		$tx.observe( $tom.collect(elContext, "div.tx-col-field a.tx-btn-add"), "click", function(){
 			colInput.value = _self.colSize = calculator.calculate(_self.colSize, Trex.Menu.Table.MAX_COL, 1);
 			return _FALSE;
 		});
-		$tx.observe($tom.collect(elContext, "div.tx-col-field a.tx-btn-sub"), "click", function(){ 
+		$tx.observe($tom.collect(elContext, "div.tx-col-field a.tx-btn-sub"), "click", function(){
 			colInput.value = _self.colSize = calculator.calculate(_self.colSize, Trex.Menu.Table.MAX_COL, -1);
 			return _FALSE;
 		});
-		
+
 		var rowInput = $tom.collect(elContext, "div.tx-row-field input");
-		$tx.observe(rowInput, "blur", function(){ 
+		$tx.observe(rowInput, "blur", function(){
 			rowInput.value = _self.rowSize = calculator.getValidValue(rowInput.value, _self.rowSize, Trex.Menu.Table.MAX_ROW);
 		});
-		$tx.observe($tom.collect(elContext, "div.tx-row-field a.tx-btn-add"), "click", function(){ 
+		$tx.observe($tom.collect(elContext, "div.tx-row-field a.tx-btn-add"), "click", function(){
 			rowInput.value = _self.rowSize = calculator.calculate(_self.rowSize, Trex.Menu.Table.MAX_ROW, 1);
 			return _FALSE;
 		});
-		$tx.observe($tom.collect(elContext, "div.tx-row-field a.tx-btn-sub"), "click", function(){ 
+		$tx.observe($tom.collect(elContext, "div.tx-row-field a.tx-btn-sub"), "click", function(){
 			rowInput.value = _self.rowSize = calculator.calculate(_self.rowSize, Trex.Menu.Table.MAX_ROW, -1);
 			return _FALSE;
 		});
@@ -418,14 +429,14 @@ Trex.Menu.Table = Trex.Class.create({
 		var elDiv = tx.div();
 		var elAConfirm = tx.a({href:"javascript:;", className:"tx-btn-confirm"}, "확인");
 		var elACancel = tx.a({href:"javascript:;", className:"tx-btn-cancel"}, "취소");
-		
+
 		$tx.observe( elAConfirm, "click", function(ev){
 			_self.onSelect(ev, {
 				row: _self.rowSize,
 				col: _self.colSize
 			});
 		});
-		
+
 		$tx.observe(elACancel, "click", function() {
 			this.onCancel();
 			return _FALSE;
