e04cc6119101492f4d22564b9ea809f644e9a837
kim hyuna <aprilhyuna@gmail.com>
2014-09-04 18:33:00 +0900

    DropZone canvas mode change bug fix  #FTDUEDTR-1458

--- daumeditor/js/trex/modules/dropzone.js
+++ daumeditor/js/trex/modules/dropzone.js
@@ -10,6 +10,83 @@ Trex.module('register drag and drop attacher on canvas', function(editor, toolba
     };
 });
 
+Trex.register("filter > dropzone",
+    function(editor) {
+        function toRegExp(str){
+            return str.replace(/([\\^$(){}[\]+*?.])/g,'\\$1');
+        }
+
+        function removeWaiting(_content) {
+            return _content.replace(getHTMLWaitingRegExp(),'').replace(getSourceWaitingRegExp(), '');
+        }
+
+        function getHTMLWaitingRegExp() {
+            return RegExp('<img[^>]*'+toRegExp(Trex.__WAITING_IMG_SRC)+'[^>]*id=[\'"](.*)[\'"][^>]*>','g')
+        }
+
+        function getSourceWaitingRegExp() {
+            return RegExp('\\[Uploading:([a-zA-Z\\d_]*)\\]', 'g');
+        }
+
+        function changeWaitingSourceToHTML(contents) {
+            var dropZone = editor.getDropZone();
+
+            return contents.replace(getSourceWaitingRegExp(), function(match, p1, offset) {
+                var imgContents = dropZone.getTempImageContents(p1);
+                if(imgContents == _NULL) {
+                    return dropZone.createIndicator(p1);
+                }
+                var _entry = _attachBox.getEntries().find(function(entry) {
+                    return (entry.data.dispElId == p1);
+                });
+                _entry && _entry.setExistStage(true);
+                return imgContents;
+            })
+        }
+
+        var _attachBox = editor.getAttachBox();
+        var _docparser = editor.getDocParser();
+        _docparser.registerFilter(
+            'filter/dropzone', {
+                'source@load': function(contents){
+                    return removeWaiting(contents);
+                },
+                'html@load': function(contents){
+                    return removeWaiting(contents);
+                },
+                'source4save': function(contents){
+                    contents = changeWaitingSourceToHTML(contents);
+                    return removeWaiting(contents);
+                },
+                'html4save': function(contents){
+                    return removeWaiting(contents);
+                },
+                'text4save': function(contents){
+                    return contents;
+                },
+                'html2text': function(contents) {
+                    return removeWaiting(contents);
+                },
+                'source2text': function(contents) {
+                    return removeWaiting(contents);
+                },
+                'source2html': function(contents){
+                    if (!editor.hasOwnProperty('getDropZone')) {
+                        return contents;
+                    }
+                    return changeWaitingSourceToHTML(contents);
+                },
+                'html2source': function(contents){
+                    if (!editor.hasOwnProperty('getDropZone')) {
+                        return contents;
+                    }
+                    return contents.replace(getHTMLWaitingRegExp(), function(match, p1, offset) {
+                        return '[Uploading:'+p1+']';
+                    });
+                }
+            });
+})
+
 Trex.MarkupTemplate.add('module.dropZone',
         '<div style="display: table; left:0px; top:0px; width:0px; height:0px; z-index:9999; border-spacing:6px; position:absolute;">\
         <div style="display:table-row">\
@@ -27,6 +104,8 @@ Trex.DropZone = Trex.Class.create({
         this.useImageUpload = config.useImageUpload;
 
         this.dataType = ["text/html", "text/uri-list", "text/plain", "Files"];
+
+        this.imgContents = {};
 
         this.cover = this._createCover();
         this.coverShow = _FALSE;
@@ -60,6 +139,14 @@ Trex.DropZone = Trex.Class.create({
         $tx.observe(this.cover, "drop", function(ev) {
             self._dropHandler(ev);
         });
+
+        this.canvas.observeJob(Trex.Ev.__CANVAS_MODE_CHANGE, function(from, to) {
+            if (from == to) return;
+
+            if (to == Trex.Canvas.__WYSIWYG_MODE) {
+                self.imgContents = {};
+            }
+        });
     },
     _checkDragType: function(ev) {
         var dataType = this.dataType;
@@ -67,7 +154,7 @@ Trex.DropZone = Trex.Class.create({
 
         var typeIndex = -1;
 
-        if (dt) {
+        if (this.canvas.isWYSIWYG() && dt) {
             $A(dt.types).each(function(type) {
                 var index = dataType.indexOf(type);
 
@@ -79,35 +166,6 @@ Trex.DropZone = Trex.Class.create({
         }
 
         return typeIndex;
-    },
-    _dropHandler: function(ev) {
-        var self = this;
-        var processor = this.canvas.getProcessor();
-
-        var dt = ev.dataTransfer || _NULL;
-        if (!dt) {
-            return;
-        }
-
-        var typeIndex = this._checkDragType(ev);
-
-        if (typeIndex != -1) {
-            if (processor.savedRange) {
-                processor.savedRange.select();
-            }
-
-            this.hideDragArea();
-            (new Trex.Area.Select()).reset();
-
-            var type = this.dataType[typeIndex];
-
-            if (type == "Files") {
-                this.attachFiles($A(dt.files));
-            } else {
-                this.attachHtml(dt.getData(type));
-            }
-            $tx.stop(ev);
-        }
     },
     showDragArea: function() {
         if(this.coverShow) {
@@ -129,6 +187,29 @@ Trex.DropZone = Trex.Class.create({
     hideDragArea: function() {
         $tom.remove(this.cover);
         this.coverShow = _FALSE;
+    },
+    _dropHandler: function(ev) {
+        var typeIndex = this._checkDragType(ev);
+
+        if (typeIndex != -1) {
+            var processor = this.canvas.getProcessor();
+            if (processor.savedRange) {
+                processor.savedRange.select();
+            }
+
+            this.hideDragArea();
+            (new Trex.Area.Select()).reset();
+
+            var type = this.dataType[typeIndex];
+
+            var dt = ev.dataTransfer;
+            if (type == "Files") {
+                this.attachFiles($A(dt.files));
+            } else {
+                this.attachHtml(dt.getData(type));
+            }
+            $tx.stop(ev);
+        }
     },
     attachFiles: function(files) {
         if (!files || !files.length) {
@@ -156,13 +237,13 @@ Trex.DropZone = Trex.Class.create({
     },
     attachImage: function(file) {
         var self = this;
-        var indicator = this.insertIndicator();
+        var _indicatorId = this.insertIndicator();
 
         var reader = new FileReader();
         $tx.observe(reader, 'loadend', function(e) {
             var data = {};
             data.imageurl = this.result;
-            self._execAttach(data, indicator);
+            self._execAttach(data, _indicatorId);
         });
         reader.readAsDataURL(file);
     },
@@ -172,47 +253,61 @@ Trex.DropZone = Trex.Class.create({
         paster.pasteHTML(data);
     },
     insertIndicator: function() {
-        var _indicator = this.canvas.getProcessor().create('img');
-        _indicator.src = Trex.__WAITING_IMG_SRC;
-        _indicator.width = 16;
-        _indicator.height = 16;
+        var _indicatorID = Trex.Util.getDispElId();
 
-        this._insertNode(_indicator);
-
-        return _indicator;
+        this.canvas.getProcessor().pasteContent(this.createIndicator(_indicatorID), _TRUE);
+        return _indicatorID;
     },
-    _execAttach: function(data, replaceNode) {
+    createIndicator:function(id) {
+        return '<img src='+Trex.__WAITING_IMG_SRC+' width=16 height=16 id='+id+'>';
+    },
+    _getIndicator:function(id) {
+        if (this.canvas.isWYSIWYG()) {
+            var wysiwygDoc = this.canvas.getCurrentPanel().getDocument();
+            var indicator = $tom.collect(wysiwygDoc, "#" + id);
+
+            return indicator;
+        }
+        return _NULL;
+    },
+    _removeIndicator: function(id){
+        if (!id || id == "") {
+            return;
+        }
+        if (this.canvas.isWYSIWYG()) {
+            self._getIndicator(id);
+
+            if (indicator) {
+                goog.dom.removeNode(indicator);
+            }
+        } else {
+            this.imgContents[id] = "";
+        }
+    },
+    _execAttach: function(data, replaceNodeId) {
+        var self = this;
         if (data.hasOwnProperty('imageurl')) {
+
             this.canvas.execute(function(processor) {
-                var _img = processor.create('img');
+                var _img = _DOC.createElement('img');
                 _img.src = data.imageurl;
                 $tom.addStyles(_img, {clear:'none', float: 'none'});
 
-                var parent = replaceNode ? replaceNode.parentNode || _NULL : _NULL;
+                if (self.canvas.isWYSIWYG()) {
+                    var replaceNode = self._getIndicator(replaceNodeId);
 
-                if (parent) {
-                    parent.replaceChild(_img, replaceNode);
-                } else {
-                    processor.pasteNode(_img, _TRUE);
+                    if (replaceNode) {
+                        goog.dom.replaceNode(_img, replaceNode);
+                    }
+                }
+                else {
+                    self.imgContents[replaceNodeId] = goog.dom.getOuterHtml(_img);
                 }
             });
         }
     },
-    _insertNode: function(node) {
-        var processor = this.canvas.getProcessor();
-        var range = processor.createGoogRange();
-
-        if (range == _NULL) {
-            processor.focusOnTop();
-            range = processor.createGoogRange();
-        }
-
-        if (!range.isCollapsed()) {
-            range.removeContents();
-            range.select();
-        }
-
-        processor.pasteNode(node, _TRUE);
+    getTempImageContents:function(id) {
+        return this.imgContents[id] || _NULL;
     },
     alertMessage: function(overfiles) {
         if (!overfiles || overfiles.length == 0) {
